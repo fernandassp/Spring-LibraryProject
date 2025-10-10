c9c7688a483873391fc5ab01da4e10219c483c50
fernandassp <fernanda1907@gmail.com>
2025-10-10 11:02:59 -0300

    criação projeto e classe User

---
+++ src/main/java/com/libraryProject/domain/User.java
@@ -0,0 +1,5 @@
+package com.libraryProject.domain;
+
+public class User {
+
+}
---
+++ src/test/java/com/libraryProject/SpringLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
